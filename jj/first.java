fe9b987ca37d3533c7bd2d6c4c0a0961c061d149
obidjon0901 <obidjon0901>
2025-10-24 14:14:46 +0500

    frist commit

---
+++ jj/first.java
@@ -0,0 +1,6 @@
+public class firstjava{
+    Run Debug
+    public static void main(string[] args){
+        System.out.println(x:"hello world");
+    }
+}
---
+++ script/script.js
@@ -0,0 +1,8 @@
+let fullname = "tony";
+let age = 24;
+let totalprice = 1399;
+ console.log(fullname);
+console.log(age);
+console.log(totalprice);
+
+
